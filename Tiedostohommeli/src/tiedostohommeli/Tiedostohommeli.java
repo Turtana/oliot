b8edc3095f7a67d6e9cf06248dd74239caa43952
k4873 <k4873@lut1825.pc.lut.fi>
2016-06-14 14:25:03 +0300

    Eka

---
+++ Tiedostohommeli/src/tiedostohommeli/Tiedostohommeli.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tiedostohommeli;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author k4873
+ */
+public class Tiedostohommeli {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    
+    
+   
+    public static void main(String[] args) throws IOException {
+        Ioput io = new Ioput();
+        io.lueZip("zipinput.zip");
+        
+        
+    }
+    
+}
